62a631ca9cb8d3585b8d16ecf6cc0b2d3a712278
Summer <nathandouglaslunn@gmail.com>
2022-11-16 17:46:24 -0500

    app listen for process.env.PORT

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 
+const PORT = process.env.PORT || 8000;
+
 const userRouter = require('./routes/user-router.js');
 const postRouter = require('./routes/post-router.js');
 
 app.use('/users', userRouter);
 app.use('/posts', postRouter)
 
-app.listen(8000)
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+})
